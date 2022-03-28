1b47f849a0b0bec8129fd9b295d5ffc491c0c691
Varun Chilukuri <varunch321@gmail.com>
2022-03-28 14:07:55 -0400

    Store results in a lookup table

--- check_classes.py
+++ check_classes.py
@@ -1,8 +1,8 @@
 from helium import *
 from time import sleep
 
-class_list = {"CMSC132" : ["0101", "0102"], "CMSC216" : ["0301", "0909"], "CMSC000" : ["0101"]}
-
+class_list = {"CMSC330" : ["0101", "0103"], "CMSC351" : ["0201"], "INAG110" : ["0501", "0505"]}
+seat_list = {}
 start_firefox("https://app.testudo.umd.edu/soc/", headless=True)
 
 for a in class_list.keys():
@@ -13,6 +13,9 @@ for a in class_list.keys():
         wait_until(lambda: Text(to_right_of="Open:").exists or Text("No courses matched your search filters above.").exists)
         if Text("No courses matched your search filters above.").exists():
             print("Section or Course doesn't exist.")
+            seat_list[a+"-"+b] = 0
         else:
             print(Text(to_right_of="Open:").value)
+            seat_list[a+"-"+b] = int(Text(to_right_of="Open:").value)
 kill_browser()
+print(seat_list)
