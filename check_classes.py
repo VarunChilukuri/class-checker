117c78a66984499e1e87b34183bce60347f511e4
Varun Chilukuri <varunch321@gmail.com>
2022-04-07 21:03:40 -0400

    Handle multiple inputted section numbers

--- check_classes.py
+++ check_classes.py
@@ -17,17 +17,17 @@ def email_form():
 
 def parse_course_input(c1, s1, c2, s2, c3, s3, c4, s4, c5, s5, c6, s6):
     if c1 != '':
-        class_list[c1] = [s1]
+        class_list[c1] = s1.split(",")
     if c2 != '':
-        class_list[c2] = [s2]
+        class_list[c2] = s2.split(",")
     if c3 != '':
-        class_list[c3] = [s3]
+        class_list[c3] = s3.split(",")
     if c4 != '':
-        class_list[c4] = [s4]
+        class_list[c4] = s4.split(",")
     if c5 != '':
-        class_list[c5] = [s5]
+        class_list[c5] = s5.split(",")
     if c6 != '':
-        class_list[c6] = [s6]
+        class_list[c6] = s6.split(",")
 
 def get_seats(class_list):
     #class_list = {"CMSC330" : ["0101", "0103"], "CMSC351" : ["0201"], "INAG110" : ["0501", "0505"]}
